4fbe6203b0eb195b13454b939ddf5b61d4a5d663
CarterDugan <cdugan@mtu.edu>
2022-05-14 15:46:59 -0400

    Added TODOs

--- interpreter/bfrun.c
+++ interpreter/bfrun.c
@@ -1,3 +1,9 @@
+/*
+TODO: Complete interpreter
+TODO: Options for output byte representation (numeric, character)
+TODO: Runtime errors
+*/
+
 #include <stdio.h>
 
 int main(int argc, char** argv) {
